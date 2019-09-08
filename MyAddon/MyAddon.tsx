beaa1075758a4772fabc3cd0420c8330d4ca4c26
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-08 09:11:23 -0700

    Remove unused App

--- MyAddon/MyAddon.tsx
+++ MyAddon/MyAddon.tsx
@@ -1,7 +1,5 @@
 import * as Didact from './Lib/didact/didact';
 
-import { App } from './App';
-
 const rootFrame = CreateFrame('Frame', undefined, UIParent);
 rootFrame.SetPoint('CENTER', UIParent, 'CENTER', -200, 0);
 rootFrame.SetSize(300, 300);
