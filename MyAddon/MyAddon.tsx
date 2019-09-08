3fdda7fe10199798991c0d76064ce44b7dc66e7b
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-08 14:12:21 -0700

    Basic frame test

--- MyAddon/MyAddon.tsx
+++ MyAddon/MyAddon.tsx
@@ -1,7 +1,12 @@
 import * as Didact from './Lib/didact/didact';
 
-const rootFrame = CreateFrame('Frame', undefined, UIParent);
-rootFrame.SetPoint('CENTER', UIParent, 'CENTER', -200, 0);
-rootFrame.SetSize(300, 300);
+const frame = CreateFrame('Frame', 'tsCoolDown_UIParent', UIParent);
+frame.SetPoint('CENTER', UIParent, 'CENTER', -200, 0);
+frame.SetSize(300, 300);
 
-Didact.render(<frame Points={['CENTER']} />, rootFrame);
+Didact.render(
+  <frame name="tsCoolDown_test">
+    <font-string Text="Test" />
+  </frame>,
+  frame
+);
