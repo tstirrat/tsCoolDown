91174a5077c023e41b414f532f3e6bbd26ad177b
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-08 21:46:31 -0700

    Attempt to use <Icon />

--- MyAddon/MyAddon.tsx
+++ MyAddon/MyAddon.tsx
@@ -1,12 +1,15 @@
 import * as Didact from './Lib/didact/didact';
+import { Icon } from './Components/Icon';
 
 const frame = CreateFrame('Frame', 'tsCoolDown_UIParent', UIParent);
 frame.SetPoint('CENTER', UIParent, 'CENTER', -200, 0);
 frame.SetSize(300, 300);
 
+const FONT: JSX.Font = ['Fonts\\FRIZQT__.TTF', 22];
+
 Didact.render(
   <frame name="tsCoolDown_test">
-    <font-string Text="Test" />
+    <Icon texture="Interface\Icons\INV_Potion_54" />
   </frame>,
   frame
 );
