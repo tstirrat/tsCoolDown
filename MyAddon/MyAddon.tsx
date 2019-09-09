684282d13b84348081edbf9e1945138764568be3
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-09 09:38:59 -0700

    Simple cooldown test

--- MyAddon/MyAddon.tsx
+++ MyAddon/MyAddon.tsx
@@ -1,15 +1,28 @@
 import * as Didact from './Lib/didact/didact';
-import { Icon } from './Components/Icon';
+import { Cooldown, Props } from './Components/Cooldown';
 
 const frame = CreateFrame('Frame', 'tsCoolDown_UIParent', UIParent);
 frame.SetPoint('CENTER', UIParent, 'CENTER', -200, 0);
 frame.SetSize(300, 300);
 
-const FONT: JSX.Font = ['Fonts\\FRIZQT__.TTF', 22];
+const t: Props = {
+  textures: [
+    'Interface\\Icons\\INV_Potion_54',
+    'Interface\\Icons\\INV_Potion_76'
+  ],
+  start: GetTime(),
+  duration: 60 * 3,
+  name: 'tsCoolDown_Test_Timer'
+};
 
 Didact.render(
   <frame name="tsCoolDown_test">
-    <Icon texture="Interface\Icons\INV_Potion_54" />
+    <Cooldown
+      start={t.start}
+      duration={t.duration}
+      name={t.name}
+      textures={t.textures}
+    />
   </frame>,
   frame
 );
