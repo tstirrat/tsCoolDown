a023cebc260fd459955f9daa28f191765afdede1
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-09 17:49:39 -0700

    fix up time formatting

--- tsCoolDown/utils/time.ts
+++ tsCoolDown/utils/time.ts
@@ -1,16 +1,16 @@
 /** @noSelfInFile */
 function fmod(a: number, b: number) {
   return Number((a - Math.floor(a / b) * b));
-};
+}
 
 export function formatRemainingTime(secsRemaining: number) {
   if (secsRemaining <= 0) {
     return '00:00';
   }
-  const min = Math.floor(secsRemaining / 60);
-  const sec = Math.floor(fmod(secsRemaining, 60));
+  const min = `${Math.floor(secsRemaining / 60)}`.padStart(2, '0');
+  const sec = `${Math.floor(fmod(secsRemaining, 60))}`.padStart(2, '0');
   return `${min}:${sec}`;
-};
+}
 
 export function getRemainingTime(start: number, duration: number) {
   return start + duration - GetTime();
@@ -18,12 +18,12 @@ export function getRemainingTime(start: number, duration: number) {
 
 export function throttle(fn: CallableFunction, threshold: number) {
   assert(threshold, 'threshold is required');
-  let t = 0;
-  return function(frame: WowFrame, elapsed: number) {
-    t = t + elapsed;
-    if (t > threshold) {
-      t = 0;
-      fn(frame, elapsed);
+  let elapsed = 0;
+  return function(frame: WowFrame, frameTime: number) {
+    elapsed +=  frameTime;
+    if (elapsed > threshold) {
+      elapsed = 0;
+      fn(frame, frameTime);
     }
   };
-};
+}
