eee79fcb434b148a9d1079436024aceaff381158
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-12 00:16:23 -0700

    Restore old (working) reconcileChildren

--- tsCoolDown/Lib/didact/reconciler.ts
+++ tsCoolDown/Lib/didact/reconciler.ts
@@ -1,6 +1,7 @@
 import {Component} from './component';
 import {InternalElement, TEXT_ELEMENT} from './element';
 import {cleanupFrame, createFrame, updateFrameProperties} from './wow-utils';
+import { stringify } from '../../utils/debug';
 
 export interface Instance {
   publicInstance?: Component;
@@ -42,6 +43,7 @@ export function reconcile(
     instance.element = element;
     return instance;
   } else if (instance.publicInstance) {
+    // console.log('reconcile composite', (element.type as any).name, stringify(element.props));
     // Update composite instance
     instance.publicInstance.props = element.props;
     const childElement = instance.publicInstance.render();
@@ -62,31 +64,30 @@ export function reconcile(
   }
 }
 
-function reconcileChildren(
-    instance: Instance, element: InternalElement): Instance[] {
+function reconcileChildren(instance: Instance, element: InternalElement) {
   const hostFrame = instance.hostFrame;
   const childInstances = instance.childInstances;
   const nextChildElements = element.props.children || [];
-
-  const newChildInstances = childInstances.map((childInstance, i) => {
+  const newChildInstances = [];
+  const count = Math.max(childInstances.length, nextChildElements.length);
+  for (let i = 0; i < count; i++) {
+    const childInstance = childInstances[i];
     const childElement = nextChildElements[i];
-    return reconcile(hostFrame, childInstance, childElement);
-  });
-
-  return newChildInstances.filter(
-      (instance): instance is Instance => instance != null);
+    const newChildInstance = reconcile(hostFrame, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+  return newChildInstances.filter(instance => instance != null);
 }
 
 function instantiate(
     element: InternalElement, parentFrame: WowRegion): Instance {
   const {type, props} = element;
 
-  console.log('instantiate', type, Object.keys(props || {}).join(', '));
-
   if (typeof type === 'string') {
     if (type === TEXT_ELEMENT) {
       throw 'Cannot create inline text, yet';
     }
+    // console.log('instantiate', type, stringify(props));
 
     // Instantiate host element
     const frame =
@@ -103,7 +104,7 @@ function instantiate(
     return instance;
 
   } else {
-    console.log('instantiate Component', Object.keys(element || {}).join(', '));
+    // console.log('instantiate', (type as any).name, stringify(props));
     // Instantiate component element
     const instance = {} as Instance;
     const publicInstance = createPublicInstance(element, instance);
