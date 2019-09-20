654fc397971382f033e1771f7ac8af8c29486809
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-20 11:07:15 -0700

    Add AceConsole and basic chat command

--- tsCoolDown/tsCoolDown.tsx
+++ tsCoolDown/tsCoolDown.tsx
@@ -2,9 +2,15 @@ import * as Didact from './Lib/didact/didact';
 import { ActiveCooldowns } from './Components/ActiveCooldowns';
 import { BAR_HEIGHT, COOLDOWN_WIDTH } from './utils/constants';
 
-const tsCoolDown = LibStub('AceAddon-3.0').NewAddon('tsCoolDown');
+const tsCoolDown = LibStub('AceAddon-3.0').NewAddon(
+  'tsCoolDown',
+  'AceConsole-3.0'
+);
 
 tsCoolDown.OnInitialize = function() {
+  this.RegisterChatCommand('tsc', input => {
+    console.log('/tsc', input);
+  });
   tsCoolDown_Db = tsCoolDown_Db || {};
 
   const { x = 200, y = 200 } = tsCoolDown_Db;
--- tsCoolDown/utils/global.d.ts
+++ tsCoolDown/utils/global.d.ts
@@ -10,13 +10,16 @@ declare var tsCoolDown_Db: GlobalOptions;
 // TODO move these into @types/blah
 
 declare interface AceAddonFactory {
-  NewAddon(name: string): AceAddon;
+  NewAddon(name: string, ...embeds: string[]): AceAddon;
 }
 
 declare interface AceAddon {
   OnInitialize(): void;
   OnEnable(): void;
   OnDisable(): void;
+
+  // AceConsole-3.0
+  RegisterChatCommand(command: string, func: (this: void, args: string) => void, persist?: boolean): void;
 }
 
 
