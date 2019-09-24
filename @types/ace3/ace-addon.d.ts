2742c913d7c576b823d6fe0ea198b1a28473bf6e
Tim Stirrat <tim.stirrat@gmail.com>
2019-09-24 12:26:06 -0700

    Slight tidy of AceAddon stuff

--- @types/ace3/ace-addon.d.ts
+++ @types/ace3/ace-addon.d.ts
@@ -1,18 +1,24 @@
+/** Valid embeddable Ace mixins */
+declare type AceEmbedName = 'AceBucket-3.0' | 'AceConsole-3.0' | 'AceComm-3.0' |
+    'AceConfigCmd-3.0' | 'AceConfigDialog-3.0' | 'AceConfigRegistry-3.0' |
+    'AceConsole-3.0' | 'AceEvent-3.0' | 'AceHook-3.0' | 'AceSerializer-3.0' |
+    'AceTimer-3.0';
 
 declare interface AceAddonFactory {
-  NewAddon(name: string, ...embeds: AceMixinName[]): AceAddon;
+  NewAddon(name: string, ...embeds: AceEmbedName[]): AceAddon;
 }
-
-declare type AceMixinName = 'AceBucket-3.0' | 'AceConsole-3.0' | 'AceComm-3.0' |
-    'AceConfigCmd-3.0' | 'AceConfigDialog-3.0' | 'AceConfigRegistry-3.0' |
-    'AceConsole-3.0' | 'AceDB-3.0' | 'AceDBOptions-3.0' | 'AceEvent-3.0' |
-    'AceGUI-3.0' | 'AceHook-3.0' | 'AceLocale-3.0' | 'AceSerializer-3.0' |
-    'AceTab-3.0' | 'AceTimer-3.0';
 
 /** AceAddon-3.0 */
 declare interface AceAddon extends AceBucket, AceConfigCmd, AceConfigDialog,
                                    AceConsole, AceComm, AceDB {
+  /* Called only once, when the addon is first loaded by the game client. */
   OnInitialize(): void;
+
+  /**
+   * Called when the addon is enabled, may be called more than once per session
+   */
   OnEnable(): void;
+
+  /** Called when the addon is disabled */
   OnDisable(): void;
 }
